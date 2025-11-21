d46d98b254cef96090641bb21f903d840d4ce8ec
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-11-21 11:01:42 -0300

    Implementación de la consulta de las estadísticas

--- bot/command_handler.py
+++ bot/command_handler.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from services.football_data_org_api_service import FootballDataOrgApiService
-from utils.formatters import formatear_clasificacion_tabla, formatear_partidos
+from utils.formatters import formatear_clasificacion_tabla, formatear_partidos, formatear_goleadores
 
 class CommandHandlerBot:
   """
@@ -68,34 +68,9 @@ class CommandHandlerBot:
     """
     Responde al comando /estadisticas.
     """
-    await update.message.reply_html(
-      f"📈 <b>Estadísticas - 12/11/2025</b>\n\n"
-        
-      "🥅 <b>Goleadores</b>\n"
-      "1️⃣ Pablo Vegetti (Belgrano) — 12 goles\n"
-      "2️⃣ Miguel Borja (River) — 11 goles\n"
-      "3️⃣ Edinson Cavani (Boca) — 10 goles\n"
-      "4️⃣ Adrián Martínez (Racing) — 9 goles\n"
-      "5️⃣ Franco Cristaldo (Lanús) — 8 goles\n\n"
-      
-      "🎯 <b>Asistidores</b>\n"
-      "1️⃣ Cristian Ferreira (Talleres) — 8 asistencias\n"
-      "2️⃣ Ezequiel Barco (River) — 7 asistencias\n"
-      "3️⃣ Valentín Barco (Boca) — 6 asistencias\n"
-      "4️⃣ Ignacio Malcorra (Central) — 5 asistencias\n"
-      "5️⃣ Maxi Meza (Estudiantes) — 5 asistencias\n\n"
-      
-      "🟨 <b>Tarjetas Amarillas</b>\n"
-      "1️⃣ Rodrigo Aliendro (River) — 6\n"
-      "2️⃣ Damián Pérez (Arsenal) — 5\n"
-      "3️⃣ Gastón Benavídez (Talleres) — 5\n"
-      "4️⃣ Nicolás Oroz (Racing) — 4\n"
-      "5️⃣ Enzo Díaz (River) — 4\n\n"
-      
-      "🟥 <b>Tarjetas Rojas</b>\n"
-      "1️⃣ Marcos Rojo (Boca) — 2\n"
-      "2️⃣ Carlos Quintana (Lanús) — 2\n"
-      "3️⃣ Gabriel Hauche (Racing) — 1\n"
-      "4️⃣ Enzo Pérez (Estudiantes) — 1\n"
-      "5️⃣ Jonathan Galván (Huracán) — 1"
-    )
+    try:
+      goleadores = await self._api_service.obtener_estadisticas()
+      mensaje = formatear_goleadores(goleadores)
+      await update.message.reply_html(mensaje)
+    except Exception as e:
+      await update.message.reply_text(f"❌ Error obteniendo las estadísticas: {e}")
