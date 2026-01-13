28829a604b5e83e0b0340abae97af65e90bd8b00
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2026-01-13 12:43:59 -0300

    Instanciación de un servidor HTTP para mantener ejecutando el bot en Render

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import logging
+import threading
+import os
 from bot.futbot import FutBot
 from config.config import Config
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 # Configurar logging:
 logging.basicConfig(
@@ -8,12 +11,26 @@ logging.basicConfig(
   level=logging.INFO
 )
 
+def start_dummy_server():
+  class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+      self.send_response(200)
+      self.end_headers()
+      self.wfile.write(b"OK")
+
+  port = int(os.environ.get("PORT", 10000))
+  logging.info(f"🌐 Dummy HTTP server escuchando en puerto {port}")
+  server = HTTPServer(("0.0.0.0", port), Handler)
+  server.serve_forever()
+
 # Inicializar e iniciar el bot:
 def main():
   """
   Inicializa e inicia FutBot.
   """
   try:
+    threading.Thread(target=start_dummy_server, daemon=True).start()
+    
     config = Config()
     bot = FutBot(config)
     bot.run()
